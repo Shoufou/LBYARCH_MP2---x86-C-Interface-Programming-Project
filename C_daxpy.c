44c4f3ea8724805302b8ee74da6326bf66856800
Shoufou <riverap1231@gmail.com>
2025-11-23 23:38:46 +0800

    added all files into a folder

--- C_daxpy.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void daxpy_c(int n, double A, double* vectorX, double* vectorY, double* vectorZ) {
-    for (int i = 0; i < n; i++) {
-        vectorZ[i] = A * vectorX[i] +vectorY[i];
-    }
-}
